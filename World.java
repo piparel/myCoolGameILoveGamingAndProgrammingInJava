1d651c5838a09f008625b1b189b6d2adb7630258
piparel <valdmanis299@gmail.com>
2025-01-06 15:53:17 +0100

    world works, needs special tiles still

--- World.java
+++ World.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class World {
     private static final int SIZE = 24;
     private String[][] map = new String[SIZE + 1][SIZE + 1];
-
+    Random random = new Random();
 
     private void initializeMap() {
         for (int x = 0; x <= SIZE; x++) {
@@ -11,30 +11,29 @@ public class World {
                 map[x][y] = "■";
             }
         }
-        for (String[] row : map) {
-            System.out.println(String.join("  ", row));
-        }
     }
 
     private void generateMap(){
         initializeMap();
         boolean builderSpawned = false;
-        boolean orthogonalAllowed = false;
         int builderMoveDirection = 0;
         int allocatedBlocks = 0;
         int rootX = SIZE/2; int rootY = SIZE/2;
         int stepped = 0;
 
-        while(allocatedBlocks<((SIZE * SIZE)/ 8)){
+        int cx = 0;
+        int cy = 0;
+
+        while(allocatedBlocks<((SIZE * SIZE)/ 4)){
             if (builderSpawned == false){
-                int cx = 2 + Math.floor(Math.random()* SIZE - 2);
-                int cy = 2 + Math.floor(Math.random()* SIZE - 2);
+                cx = 2 + (int) Math.floor(random.nextDouble() * (SIZE - 2));
+                cy = 2 + (int) Math.floor(random.nextDouble() * (SIZE - 2));
 
                 if (Math.abs(rootX - cx) <= 0 && Math.abs(rootY - cy) <= 0) {
-                    if (map[cx][cy] != "■") {
+                    if (map[cx][cy] != "·") {
                         map[cx][cy] = "·";
                         allocatedBlocks++;
-                    } // end if
+                    }
                 } else {
                     builderSpawned = true;
                     Random random = new Random();
@@ -42,14 +41,31 @@ public class World {
                     stepped = 0;
                 }
             }
+            else {
+					if (builderMoveDirection==0 && cy>0){cy--;stepped++;}
+					else if (builderMoveDirection==1 && cx<SIZE){cx++;stepped++;}
+					else if (builderMoveDirection==2 && cy<SIZE){cy++;stepped++;}
+					else if (builderMoveDirection==3 && cx>0){cx++;stepped++;}
+					else if (builderMoveDirection==4 && cx<SIZE && cy>0){cy--;cx++;stepped++;}
+					else if (builderMoveDirection==5 && cx<SIZE && cy<SIZE){cy++;cx++;stepped++;}
+					else if (builderMoveDirection==6 && cx>0 && cy<SIZE){cy++;cx--;stepped++;}
+					else if (builderMoveDirection==7 && cx>0 && cy>0){cy--;cx--;stepped++;}
+
+					if (cx<SIZE && cy<SIZE && cx>1 && cy>1 && stepped<=7){
+					if (map[cx+1][cy]=="·"){if (map[cx][cy]!="·"){map[cx][cy]="·";allocatedBlocks++;}
+					} else if (map[cx-1][cy]=="·"){if (map[cx][cy]!="·"){map[cx][cy]="·";allocatedBlocks++;} 
+					} else if (map[cx][cy+1]=="·"){if (map[cx][cy]!="·"){map[cx][cy]="·";allocatedBlocks++;}
+					} else if (map[cx][cy-1]=="·"){if (map[cx][cy]!="·"){map[cx][cy]="·";allocatedBlocks++;}
+					}} else { builderSpawned=false;}
+            }
+        }
+        for (String[] row : map) {
+            System.out.println(String.join("  ", row));
         }
     }
 
-
-    
-
     public static void main(String[] args) {
         World map = new World();
-        map.initializeMap();
+        map.generateMap();
     }
 }
